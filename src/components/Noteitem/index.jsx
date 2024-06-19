50b44ea720dda1fa50553d626c2bb6c3ebe22d1b
Karine <karinepratesda@gmail.com>
2024-06-19 15:10:55 -0300

    Back-end integration with front-end

---
+++ src/components/Noteitem/index.jsx
@@ -0,0 +1,31 @@
+import {FiPlus, FiX} from 'react-icons/fi';
+
+import { Container } from './styles';
+
+export function NoteItem ({isNew, value, onClick, ...rest}) {
+
+  return (
+
+    <Container>
+
+      <input 
+      type="text"
+      value={value}
+      readOnly={!isNew}
+      {...rest}
+      />
+    <button
+      type='button'
+      className={isNew ? 'button-add' : 'button-delete'}  
+      onClick={onClick}
+    >
+      { isNew ? <FiPlus  /> : <FiX/>}
+    
+    </button>
+      
+
+    </Container>
+  )
+
+
+}
